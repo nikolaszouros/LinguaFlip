75c1ca1cd42080faf79e07ac512ff73a9e51b2d9
I6386110 <n.zouros@student.maastrichtuniversity.nl>
2026-04-12 00:43:13 +0200

    change in ai service, ai service does not work fully yet

--- linguaflip/services/ai_service.py
+++ linguaflip/services/ai_service.py
@@ -11,8 +11,8 @@ def _lang_name(code):
 
 
 class AIService:
-    def __init__(self, api_key):
-        self.api_key = os.environ.get('GEMINI_API_KEY')
+    def __init__(self, api_key=None):
+        self.api_key = api_key or os.environ.get('GEMINI_API_KEY')
         if self.api_key:
             self.client = genai.Client(api_key=self.api_key)
         else:
